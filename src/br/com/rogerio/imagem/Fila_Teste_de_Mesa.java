b4f22a5ac1b06a8398cec3eecab514a07a781f4c
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-10-20 21:27:21 -0300

    Primeiro commit do Projeto Fila Exercicio 06 Teste de Mesa

---
+++ src/br/com/rogerio/imagem/Fila_Teste_de_Mesa.java
@@ -0,0 +1,32 @@
+/*
+ *6. Desenhe o array q interno da classe Fila após as operações do código
+abaixo. Não se esqueça de representar o (1) tamanho da array, (2) os seus
+elementos e as (3) posições em que se encontram estes elementos. Caso
+não seja possível realizar alguma operação, ignore-a.
+main()
+{
+Fila q = new Fila(8);
+q.pop();
+q.push(1);
+q.push(2);
+q.push(3);
+q.push(4);
+q.push(5);
+q.pop();
+q.pop();
+q.pop();
+q.push(6);
+q.push(7);
+q.push(8);
+q.push(9);
+}
+ */
+package br.com.rogerio.imagem;
+
+/**
+ *
+ * @author roger
+ */
+public class Fila_Teste_de_Mesa {
+    
+}
